73f030ff6351f6d259f245a5353bff1790949ae4
reed-foster <reed.a.foster@gmail.com>
2016-12-11 15:47:13 -0800

    working display

--- New Physics Simulator/src/com/foster/physics/Main.java
+++ New Physics Simulator/src/com/foster/physics/Main.java
@@ -1,6 +1,9 @@
 package com.foster.physics;
 
 import java.awt.Graphics;
+//import java.awt.Graphics2D;
+//import java.awt.RenderingHints;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -9,7 +12,7 @@ class Main extends JPanel
 {
 	Main main;
 	
-	Environment environment;
+	static Environment environment;
 	
 	@Override
 	public void paint(Graphics g)
@@ -18,29 +21,42 @@ class Main extends JPanel
 		environment.paintall(g);
 	}
 	
-	public void update()
+	public void update(Environment e)
 	{
-		
+		e.integrateAll();
+		e.collideAll();
+		//System.out.println("Hello, world!");
 	}
 	
-	public void loop()
+	public void loop(Environment e)
 	{
-		main.update();
-		main.repaint();
+		update(e);
+		repaint();
 	}
 	
 	public static void main(String[] args) throws InterruptedException
 	{
+		environment = new Environment();
 		JFrame frame = new JFrame("Physics Simulator");
 		Main main = new Main();
 		frame.add(main);
-		frame.setSize(800, 600);
+		frame.setSize(Environment.dispwidth, Environment.dispheight + 40);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		Circle circle1 = new Circle(10, new Vector(50, 500), 50);
+		//Circle circle2 = new Circle(5, new Vector(200, 300), 20);
+		Circle circle3 = new Circle(10, new Vector(50, 380), 50);
+		
+		environment.newEntity(circle1);
+		//environment.newEntity(circle2);
+		environment.newEntity(circle3);
+		
+		circle1.addForce(new Vector(0, -20));
+		
 		while (true)
 		{
-			main.loop();
+			main.loop(environment);
 			Thread.sleep(10);
 		}
 	}
