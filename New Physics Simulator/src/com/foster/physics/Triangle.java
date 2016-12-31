72302b721cb9f551a7d1eda65dc1520088942d2b
reed <reed@localhost>
2016-12-30 17:26:07 -0800

    Used a private array of vectors instead of 3 vectors to store triangle

--- New Physics Simulator/src/com/foster/physics/Triangle.java
+++ New Physics Simulator/src/com/foster/physics/Triangle.java
@@ -6,14 +6,40 @@ package com.foster.physics;
  */
 public class Triangle
 {
-	Vector p1;
-	Vector p2;
-	Vector p3;
+	private Vector[] vertices;
 	
+	/**Constructor for triangles
+	 * @param p1 = 1st point vector (from origin)
+	 * @param p2 = 2nd vector
+	 * @param p3 = 3rd vector
+	 */
 	Triangle(Vector p1, Vector p2, Vector p3)
 	{
-		this.p1 = p1;
-		this.p2 = p2;
-		this.p3 = p3;
+		this.vertices = new Vector[3];
+		this.vertices[0] = p1;
+		this.vertices[1] = p2;
+		this.vertices[2] = p3;
+	}
+	
+	/**Get the Vector of the vertex specified by idx
+	 * @param idx = index of the desired vector [0,2]
+	 * @return vertices[idx]
+	 */
+	Vector getp(int idx)
+	{
+		if (idx > 2 || idx < 0)
+			System.err.println("Attempt to reference an invald triangle vertex");
+		return vertices[idx];
+	}
+	
+	/**Sets the Vector of the vertex specified by idx
+	 * @param idx = index of desired vector [0,2]
+	 * @param p1
+	 */
+	void setp(int idx, Vector p1)
+	{
+		if (idx > 2 || idx < 0)
+			System.err.println("Attempt to reference an invald triangle vertex");
+		this.vertices[idx] = p1;
 	}
 }
