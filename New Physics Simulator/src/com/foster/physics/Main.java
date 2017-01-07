ed06ad1fa69a379c8ebf2fd38f9f25a7ed2405bb
reed <reed@localhost>
2017-01-06 18:26:08 -0800

    separated polygon generation from main method

--- New Physics Simulator/src/com/foster/physics/Main.java
+++ New Physics Simulator/src/com/foster/physics/Main.java
@@ -37,31 +37,23 @@ class Main extends JPanel
 	
 	public void addcircles()
 	{
-		for(int i = 0; i <= 20; i++)
+		for(int i = 0; i <= 150; i++)
 		{
 			double density = 5;//(Math.random() + 2) * 5;
 			Vector position = new Vector((Math.random() + 0.01) * (Environment.dispwidth - 20), (Math.random() + 0.01) * (Environment.dispheight - 20));
 			double radius = (Math.random() + 0.2) * 20;
-			double restitution = 1;//(Math.random() + 9) / 10;
-			Circle newcirc = new Circle(density * Math.PI * radius * radius, position, 0, 1, restitution, radius);
+			double restitution = (Math.random() + 9) / 10;
+			Circle newcirc = new Circle(density * Math.PI * radius * radius, position, 0, 0, restitution, radius*2);
 			environment.newEntity(newcirc);
 			Vector rand_vel = new Vector((Math.random() - 0.5) * 50, (Math.random() - 0.5) * 50);
 			newcirc.vel = rand_vel.get();
-			newcirc.addForce(new Vector(0, -9.8 * newcirc.mass));
+			//newcirc.addForce(new Vector(0, -9.8 * newcirc.mass));
 		}
 	}
 	
-	public static void main(String[] args) throws InterruptedException
+	public void addpolygons()
 	{
-		environment = new Environment();
-		JFrame frame = new JFrame("Physics Simulator");
-		Main main = new Main();
-		frame.add(main);
-		frame.setSize(Environment.dispwidth, Environment.dispheight);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		/*Vector[] vertlist = new Vector[16];
+		Vector[] vertlist = new Vector[16];
 		vertlist[0] = new Vector(0, -20);
 		vertlist[1] = new Vector(20, 60);
 		vertlist[2] = new Vector(120, 60);
@@ -78,35 +70,30 @@ class Main extends JPanel
 		vertlist[13] = new Vector(-100, -140);
 		vertlist[14] = new Vector(-120, -20);
 		vertlist[15] = new Vector(-60, 40);
-		/*
-		Vector[] vertlist = new Vector[10];
-		vertlist[0] = new Vector(0,80);
-		vertlist[1] = new Vector(40,20);
-		vertlist[2] = new Vector(120,100);
-		vertlist[3] = new Vector(130, 10);
-		vertlist[4] = new Vector(80, -5);
-		vertlist[5] = new Vector(30,-160);
-		vertlist[6] = new Vector(0, -50);
-		vertlist[7] = new Vector(-80,0);
-		vertlist[8] = new Vector(-30, -10);
-		vertlist[9] = new Vector(-100, 30);*/
-		//Polygon p = new Polygon(1, new Vector(400, 300), vertlist);
+		Polygon p = new Polygon(1, new Vector(400, 300), vertlist);
+		environment.newEntity(p);
+	}
+	
+	public static void main(String[] args) throws InterruptedException
+	{
+		environment = new Environment();
+		JFrame frame = new JFrame("Physics Simulator");
+		Main main = new Main();
+		frame.add(main);
+		frame.setSize(Environment.dispwidth, Environment.dispheight);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		main.addcircles();
-		/*Vector[] vertlist = new Vector[5];
-		for(int i = 0; i < 5; i++)
-		{
-			vertlist[i] = new Vector(50*Math.sin(i * 2 * Math.PI/5), 50*Math.cos(i * 2 * Math.PI/5));
-		}
-		Polygon p = new Polygon(1, new Vector(500, 500), vertlist);
-		//p.omega = 5;*/
-		//p.addForce(new Vector(5, 0), new Vector(0, 10));
-		//p.addForce(new Vector(-5, 0), new Vector(0, -10));
-		//environment.newEntity(p);
+		//main.addpolygons();
 		
 		while (true)
 		{
 			main.loop(environment);
-			Thread.sleep(0, 10);
+			Thread.sleep(0, 1);
+			Double x = environment.circles.get(0).pos.getx();
+			if (x.isNaN())
+				break;
 		}
 	}
 }
